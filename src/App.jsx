997dc14662bb751a26250a8ea19b758630e06ecd
yeiner <ys.lopez28@ciaf.edu.co>
2025-11-12 20:42:21 -0500

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+export const App = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const testApi = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/");
+        setData(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  })
+  return (
+    <div className="flex justify-center items-center h.screen bg-gray-500">
+      {data && <code>{JSON.stringify(data.message)}</code>}
+    </div>
+  );
+}   
